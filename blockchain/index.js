68e576bae8b7f87f2ce7f4a9e8dc11fcc15c0d86
krokowski0x <krokowski.dx@gmail.com>
2018-07-17 21:51:24 +0200

    Added course version of the blockchain

---
+++ blockchain/index.js
@@ -0,0 +1,39 @@
+const Block = require('./block');
+
+class Blockchain {
+  constructor() {
+    this.chain = [Block.genesis()];
+  };
+
+  addBlock(data) {
+    const block = Block.mineBlock(this.chain[this.chain.length - 1], data);
+    this.chain.push(block);
+
+    return block;
+  };
+
+  isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
+      return false;
+
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i];
+      const lastBlock = chain[i - 1];
+
+      if (block.lastHash !== lastBlock.hash || block.hash !== Block.blockHash(block))
+        return false;
+    }
+    return true;
+  };
+
+  replaceChain(newChain) {
+    if (newChain.length <= this.chain.length)
+      throw new Error('Recived chain is not longer than the current chain.')
+    else if (!this.isValidChain(newChain))
+      throw new Error('Recieved chain is not valid.');
+    else
+      this.chain = newChain;
+  };
+};
+
+module.exports = Blockchain;
