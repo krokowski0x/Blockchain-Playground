4368e48dc830c89f55500e9b5e14bb3b54958bdf
krokowski0x <krokowski.dx@gmail.com>
2018-07-20 10:43:11 +0200

    Added course version of Transaction Pool

--- wallet/index.js
+++ wallet/index.js
@@ -1,4 +1,5 @@
 const { INITIAL_BALANCE } = require('../config.js');
+const Transaction = require('./transaction');
 const ChainUtil = require('../chain-util');
 
 class Wallet {
@@ -16,6 +17,22 @@ class Wallet {
 
   sign(dataHash) {
     return this.keyPair.sign(dataHash);
+  };
+
+  createTransacton(recipient, amount, transactionPool) {
+    if (amount > this.balance)
+      throw new Error('You ain\'t got enough money in your wallet.');
+
+    let transaction = transactionPool.existingTransaction(this.publicKey);
+
+    if (transaction)
+      transaction.update(this, recipient, amount);
+    else {
+      transaction = Transaction.newTransaction(this, recipient, amount);
+      transactionPool.updateOrAddTransaction(transaction);
+    }
+
+    return transaction;
   };
 };
 
