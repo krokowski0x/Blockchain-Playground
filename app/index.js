92d4afe9b79cc83df5c41bfd9cefbcf62c59b318
krokowski0x <krokowski.dx@gmail.com>
2018-07-20 16:01:08 +0200

    Added more API endpoints and additional P2P Server functionality

--- app/index.js
+++ app/index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2PServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
+const Miner = require('./miner');
 
-const HTTP_PORT = process.env.HTTP_PORT || 3000;
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
-const p2pServer = new P2PServer(bc);
+const wallet = new Wallet();
+const tp = new TransactionPool();
+const p2pServer = new P2PServer(bc, tp);
+const miner = new Miner(bc, tp, wallet, p2pServer);
 
 app.use(bodyParser.json());
 
@@ -17,11 +23,33 @@ app.get('/blocks', (req, res) => {
 
 app.post('/mine', (req, res) => {
   const block = bc.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
 
   p2pServer.syncChains();
 
   res.redirect('/blocks');
 });
 
-app.listen(HTTP_PORT, () => {console.log(`App is running on port ${HTTP_PORT}`)});
+app.get('/transactions', (req, res) => {
+  res.json(tp.transactions);
+});
+
+app.post('/transact', (req, res) => {
+  const { recipient, amount } = req.body;
+  const transaction = wallet.createTransaction(recipient, amount, bc, tp);
+  p2pServer.broadcastTransaction(transaction);
+  res.redirect('/transactions');
+});
+
+app.get('/mine-transactions', (req, res) => {
+  const block = miner.mine();
+  console.log(`New block added: ${block.toString()}`);
+  res.redirect('/blocks');
+});
+
+app.get('/public-key', (req, res) => {
+  res.json({ publicKey: wallet.publicKey });
+});
+
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
 p2pServer.listen();
