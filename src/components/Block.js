2e8c48567b6e2f7dcc6e11b596efb6456e4078fd
krokowski0x <krokowski.dx@gmail.com>
2018-07-31 23:18:53 +0200

    Refactored Block component to class-based

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import Block from '../../blockchain/block';
+import React, { Component } from "react";
+import posed from "react-pose";
+import styled from "styled-components";
 
-const Block = () => {
-  return (
-    <div className="Block">
+const Square = posed.div({
+  idle: { scale: 1 },
+  hovered: { scale: 1.5 }
+});
 
-    </div>
-  );
+const StyledSquare = styled(Square)`
+  text-align: center;
+  display: inline-block;
+  padding: 2em;
+  margin-right: 1em;
+  font-weight: bold;
+  font-size: 1.5em;
+  border-radius: 10px;
+  background: transparent;
+  color: black;
+  border: 2px solid black;
+`;
+
+export default class Block extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovering: false,
+    };
+  };
+
+  render() {
+    return (
+      <StyledSquare
+        pose={this.state.hovering ? "hovered" : "idle"}
+        onMouseEnter={() => this.setState({ hovering: true })}
+        onMouseLeave={() => this.setState({ hovering: false })}
+      > Block
+      </StyledSquare>
+    );
+  }
 };
-
-export default Block;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,45 +2,33 @@ import React, { Component } from "react";
 import posed from "react-pose";
 import styled from "styled-components";
 
+import Block from './Block';
+
 const Container = styled.div`
-  height: 100vh;
-  display: flex;
+  margin: 0vh 10vw;
+  height: 80vh;
+  width: 80vw;
   align-items: center;
   justify-content: center;
 `;
 
-const Square = posed.div({
-  idle: { scale: 1 },
-  hovered: { scale: 1.5 }
-});
 
-const StyledSquare = styled(Square)`
-  width: 100px;
-  height: 100px;
-  background: red;
-`;
-
-class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hovering: false,
-    };
+export default class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {};
   };
 
   render() {
     return (
       <Container>
-        <h1>What up</h1>
+        <h1>Blockchain Playground</h1>
         <h3>This app will help You undarstand basics of blockchain technology, as well as cryptocurrencies based on it. Let's dive in!</h3>
-        <StyledSquare
-          pose={this.state.hovering ? "hovered" : "idle"}
-          onMouseEnter={() => this.setState({ hovering: true })}
-          onMouseLeave={() => this.setState({ hovering: false })}
-        />
+        <Block />
+        <Block />
+        <Block />
+        <Block />
       </Container>
     );
   }
 };
-
-export default Dashboard;
